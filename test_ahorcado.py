4cdd6c37c6fe59dd0653b5274d793f8ac965e011
ayanuchausky <43687444+ayanuchausky@users.noreply.github.com>
2021-05-24 16:13:37 -0300

    push inicial

---
+++ test_ahorcado.py
@@ -0,0 +1,12 @@
+def palabra_insertada_a_interrogacion(palabra_a_adivinar):
+    #Agustín Conti: convierte la palabra en "?"
+    interrogaciones=""
+    for i in range(0,len(palabra_a_adivinar)):
+        interrogaciones+="?"
+    return interrogaciones
+
+def interfaz(palabra_a_adivinar):
+    #Agustín Conti: Imprime la interfaz del juego, faltan muchas cosas
+    aciertos=0
+    desaciertos=0
+    print("Palabra a adivinar:",palabra_insertada_a_interrogacion(palabra_a_adivinar), "Aciertos: ",aciertos , "Desaciertos: ",desaciertos)
