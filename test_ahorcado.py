b12c3e8549e1e6a24de15a05ee94a8ff38c3bac2
AContiSG <aeconti@fi.uba.ar>
2021-06-01 20:44:37 -0300

    Update test_ahorcado.py

--- test_ahorcado.py
+++ test_ahorcado.py
@@ -1,7 +1,7 @@
 letras_usadas=[]
 aciertos=0
 desaciertos=0
-palabra_a_adivinar = "autos"
+palabra_a_adivinar = "automovilistico" #palabra usada para testear
 
 def palabra_insertada_a_interrogacion(palabra_a_adivinar):
 #     Agustín Conti: Crea una cadena de interrogaciones igual de larga que la palabra
@@ -16,6 +16,7 @@ def palabra_insertada_a_interrogacion(palabra_a_adivinar):
 def print_interfaz(palabra_a_adivinar):
 #     Agustín Conti: Imprime la interfaz del juego
     print("Palabra a adivinar:",palabra_insertada_a_interrogacion(palabra_a_adivinar), "Aciertos: ",aciertos , "Desaciertos: ",desaciertos)
+    #falta que en caso de desacierto imprima las letras usadas incorrectas
 
 def ingrese_letra():
 #     Agustín Conti: Pide al usuario que ingrese una letra y verifica que sea apropiada
@@ -27,15 +28,24 @@ def ingrese_letra():
             print ("Ingreso inválido")
         letra_ingresada=str(input("Ingrese letra: ")).casefold()
     letras_usadas.append(letra_ingresada)
+    #falta que si se ingresa "fin" o "0" termine el juego
 
 def victoria():
 #     agregar funcion que verifique que todas las letras ya fueron adivinadas
     pass
 
+def contar_puntos():
+#     agregar funcion que cuente los aciertos y desaciertos
+    pass
+
+def derrota():
+#     agregar funcion que verifique que los desaciertos son los suficientes para perder
+    pass
+
 def main():
 #     Ejecuta el juego
     CANTIDAD_DESACIERTOS_PERDER= 8
-    while desaciertos<=CANTIDAD_DESACIERTOS_PERDER and not victoria():
+    while desaciertos<=CANTIDAD_DESACIERTOS_PERDER and not victoria() and not derrota(): #la funciones victoria y derrota no estan terminadas todavia
         print_interfaz(palabra_a_adivinar)
         ingrese_letra()
         palabra_insertada_a_interrogacion(palabra_a_adivinar)
